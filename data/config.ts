88e28072eb82af033848c485812446900197f436
squaresmile <squaresmile@protonmail.com>
2022-09-05 20:33:18 +0700

    Updated master character table

--- data/config.ts
+++ data/config.ts
@@ -1,4 +1,4 @@
 export const ASSET_FOLDER = "ynnFQcGDLfaUcGhp";
 export const BASE_ASSET_URL = `https://dugrqaqinbtcq.cloudfront.net/product/${ASSET_FOLDER}/assets/`;
 export const MASTER_CHARACTER =
-    "https://gist.githubusercontent.com/squaresmile/5344f0ccd32d52fc279e7a904cb44f2c/raw/0919e353022fbdde9373e0767484bb2f5f7c57e5/masterCharacter.csv";
+    "https://gist.githubusercontent.com/squaresmile/5344f0ccd32d52fc279e7a904cb44f2c/raw/987f6ab78a5b27ac7006241cfdb0e3d45f03652c/masterCharacter.csv";
